b61d0cdf4ff1b76f2a11d29664e93630743df2c8
faround <pillowsandskies@gmail.com>
2020-02-10 01:25:17 +0200

    ++ v0.1 +

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ const fs = require('fs');
 const files=fs.readdirSync('./');
 console.log(files);
 fs.readdir('./',function(err,files){
-if(err) console.log('Error',err);
-else console.log('Result',files);
+    if(err)
+        console.log('Error',err);
+    else
+        console.log('Result - ',files);
 });
