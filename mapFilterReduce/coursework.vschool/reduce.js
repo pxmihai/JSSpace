69b6f2c597b166f894e68bdff29a347156987d67
faround <pillowsandskies@gmail.com>
2020-03-25 13:07:29 +0200

    js++

--- mapFilterReduce/coursework.vschool/reduce.js
+++ mapFilterReduce/coursework.vschool/reduce.js
@@ -22,10 +22,7 @@ function totalVotes(arr) {
     for(let i=0;i<arr.length;i++)
     if (voters[i].voted==true)
         final++;
-
     return final;
-
-
 }
 
 let voters = [
@@ -43,3 +40,23 @@ let voters = [
     {name: 'Zack', age: 19, voted: false}
 ];
 console.log(totalVotes(voters)); // 7
+
+
+//4) Given an array of all your wishlist items, figure out how much it would cost to just buy everything at once
+
+function shoppingSpree(arr) {
+   return  arr.reduce( (total,{price}) => total +price ,0);
+
+    // return arr.reduce( (a,b)=>( {price: a.price + b.price  }));
+
+}
+
+let wishlist = [
+    { title: "Tesla Model S", price: 90000 },
+    { title: "4 carat diamond ring", price: 45000 },
+    { title: "Fancy hacky Sack", price: 5 },
+    { title: "Gold fidgit spinner", price: 2000 },
+    { title: "A second Tesla Model S", price: 90000 }
+];
+
+console.log(shoppingSpree(wishlist)); // 227005
