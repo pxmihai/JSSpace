f7a04256a2c04167b51be348de26c9433dc8a0ba
faround <pillowsandskies@gmail.com>
2020-03-25 12:29:09 +0200

    js++

--- mapFilterReduce/coursework.vschool/reduce.js
+++ mapFilterReduce/coursework.vschool/reduce.js
@@ -8,7 +8,7 @@ console.log(total([1,2,3])); // 6
 //2) Turn an array of numbers into a long string of all those numbers.
 
 function stringConcat(arr) {
-    return arr.reduce( (total,value)=>total +''+value);
+    return arr.reduce( (total,value)=> total + '' +value);
 }
 
 console.log(stringConcat([1,2,3])); // "123"
