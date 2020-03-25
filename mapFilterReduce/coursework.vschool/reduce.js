5000fae11c4d93579f1188df7d181e129f528154
faround <pillowsandskies@gmail.com>
2020-03-25 12:39:25 +0200

    js++

--- mapFilterReduce/coursework.vschool/reduce.js
+++ mapFilterReduce/coursework.vschool/reduce.js
@@ -12,3 +12,34 @@ function stringConcat(arr) {
 }
 
 console.log(stringConcat([1,2,3])); // "123"
+
+//3) Turn an array of voter objects into a count of how many people voted
+
+function totalVotes(arr) {
+//       for loop voters.length if voters[i].voted=true;
+//       voted++ return voted;
+    let final=0;
+    for(let i=0;i<arr.length;i++)
+    if (voters[i].voted==true)
+        final++;
+
+    return final;
+
+
+}
+
+let voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+];
+console.log(totalVotes(voters)); // 7
