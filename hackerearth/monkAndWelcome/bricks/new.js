3a6e8621cbd6b630eda234478faa501af34e52dd
faround <pillowsandskies@gmail.com>
2020-04-01 15:55:04 +0300

    js++

--- hackerearth/monkAndWelcome/bricks/new.js
+++ hackerearth/monkAndWelcome/bricks/new.js
@@ -6,35 +6,40 @@
 // console.log("--------");
 // main(12);
 // console.log("--------");
-main(10);
+main(1);
+main(2);
+main(3);
+main(4);
+main(5);
+main(8);
+main(3694);
 
 
 function main(input){
     console.log(input);
     let a,b,c,d=0;
 
-      for(let i=1;input>d;i++){
-            a=i;
-            b=2*a;
-            c=a+b;
-            d+=c;
-            console.log("aul: "+a);
-            console.log("beul: "+b);
-            console.log("ceul: "+c);
-            console.log("deul: "+d);
-            console.log("--------");
+    for(let i=1;input>d;i++){
+        a=i;
+        b=2*a;
+        c=a+b;
+        d+=c;
+        console.log("aul: "+a);
+        console.log("beul: "+b);
+        console.log("ceul: "+c);
+        console.log("deul: "+d);
+        console.log("--------");
 
-        }
-
-      console.log("finale " +a+" "+b+" "+c+" "+d);
-      let diff=d-input;/*131*/
-
-    if(input==d){
-        console.log("se termina full in b")
     }
- console.log(diff);
-    if( diff<=a ) console.log("se termina in a cu: "+ (c-diff))
-    if( diff>a   ) console.log("se termina in b cu: "+ (diff-a))
+
+    console.log("finale " +a+" "+b+" "+c+" "+d);
+
+
+    let r=c-(d-input);
+    if (r<=a) {
+        console.log("Se termina in a cu: "+ r)
+    }
+    else console.log("Se termina in b cu: " + (r-a));
 
 
 
