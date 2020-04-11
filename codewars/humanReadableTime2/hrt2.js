e24fa49a92a6535e8d9ee85bf1d06a4c21c90a93
faround <pillowsandskies@gmail.com>
2020-04-11 21:41:12 +0300

    hrt2++

--- codewars/humanReadableTime2/hrt2.js
+++ codewars/humanReadableTime2/hrt2.js
@@ -70,51 +70,82 @@ console.log(packed[4]);
         packed[2].hour,
         packed[3].minute,
         packed[4].second
-    ]
+    ];
+    console.log('I am the oracle '+oracle);
     let dice=[
         packed[0].year>0? 1:0,
         packed[1].day>0? 1:0,
         packed[2].hour>0? 1:0,
         packed[3].minute>0? 1:0,
         packed[4].second>0? 1:0
-
-    ]
+    ];
+    console.log('I am the dice '+dice);
     let plural=[
-        packed[0].year==0? "": packed[0].year==1? "year":"years",
-        packed[1].day==0? "": packed[1].day==1? "day":"days",
-        packed[2].hour==0? "": packed[2].hour==1? "hour":"hours",
-        packed[3].minute==0? "": packed[3].minute==1? "minute":"minutes",
-        packed[4].second==0? "": packed[4].second==1? "second":"seconds",
+        packed[0].year==0? "": packed[0].year==1? " year":" years",
+        packed[1].day==0? "": packed[1].day==1? " day":" days",
+        packed[2].hour==0? "": packed[2].hour==1? " hour":" hours",
+        packed[3].minute==0? "": packed[3].minute==1? " minute":" minutes",
+        packed[4].second==0? "": packed[4].second==1? " second":" seconds",
     ]
+    // console.log(plural);
 
-    console.log(plural);
     let sum = dice.reduce(function(a, b){
         return a + b;
     }, 0);
 
-    let readable;/*4 values fit here*/
-    console.log('I am the oracle '+oracle);
-    console.log('I am the dice '+dice);
+    let readable=[];/*4 values fit here*/
+
+
     console.log(sum);
 
     if(sum==0)  return 'now';
     if(sum==1)  readable=["","","",""];
     if(sum==2)  readable=[" and "];
-    if(sum>=2&&sum<=4)/*3-4*/
+    if(sum==3){
+        dice[4]==1?
+
+
+        if(dice[4]==1){
+            found=1;
+
+        }
         readable=[
-            dice[0]==0?"":readable[0]=", ",
+            dice[0]==0?"":", ",
             dice[1]==0?"": ", ",
-            dice[2]==0?"": ", ",
-            dice[3]==0?"":", ",
-
-
+            dice[2]==0?"": "and ",
+            dice[3]==0?"":"and",
 
         ]
+    }
+
+
     if(sum==5)  readable=[", ",", ",", "," and "];
+    console.log('I am readable'+readable)
+    switch(dice) {
+        case dice[3]==1:
+            readable[3]=" and "
+            break;
+        case dice[2]==1:
+            readable[2]=" and "
+            break;
+        case dice[1]==1:
+            readable[1]=" and "
+            break;
+        default:
+        // code block
+    }
 
 
 
 
-    return ("y"+packed[0].year+" "+"d"+packed[1].day+" "+"h"+packed[2].hour+" "+"m"+packed[3].minute+" "+"s"+packed[4].second);
+
+    return (
+        packed[0].year+  plural[0]+
+        packed[1].day+   plural[1]+
+        packed[2].hour+  plural[2]+
+        packed[3].minute+plural[3]+
+        packed[4].second+plural[4]
+    );
 
 }
+
---
+++ codewars/humanReadableTime2/play3.js
@@ -0,0 +1,7 @@
+let me=[0,0,1,1,1];
+test(me);
+function test(input){
+    let local=input;
+
+
+}
