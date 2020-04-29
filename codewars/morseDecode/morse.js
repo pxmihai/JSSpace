df1f8315d86ddddde12ec07bcefaaefbf53d947c
faround <pillowsandskies@gmail.com>
2020-04-29 17:01:41 +0300

    morseDone

--- codewars/morseDecode/morse.js
+++ codewars/morseDecode/morse.js
@@ -1,33 +1,22 @@
-//(decodeMorse('.... . -.--   .--- ..- -.. .'), 'HEY JUDE')
-let morse="−− ·− ·−· ·−· −·−−  ···· ·− −··  ·−  ·−·· ·· − − ·−·· ·  ·−·· ·− −− −··· ";/*MARRY HAD A LITTLE LAMB*/
-decodeMorse(morse);
-function decodeMorse (morseCode){
+morseCode="...---..."
+decodeMorse = function(morseCode) {
 
-    const phrase=morseCode;
-    const words=phrase.split("  ");
-    const nested_letters=words.map(w => w.split(" "));
-    let answer=[];
+    const phrase = morseCode;
+    const words = phrase.split("  ");
+    const nested_letters = words.map(w => w.split(" "));
+    let answer = "";
 
-    for (let i=0;i<words.length;i++)
-        for (let j=0;j<nested_letters[i].length;j++)
-        {
-            console.log((nested_letters[i][j]));
-            console.log(decode(nested_letters[i][j]));  /*WHAT AM I DOING WRONG*/
-            answer.push(decode(nested_letters[i][j]));
-
+    for (let i = 0; i < words.length; i++)
+        for (let j = 0; j < nested_letters[i].length; j++) {
+            // console.log((nested_letters[i][j]));
+            answer += (decode(nested_letters[i][j]));
         }
-    console.log(">> "+answer);
-    // for (let key in alphabet) {
-    //
-    //     if(alphabet.hasOwnProperty(letter)){
-    //
-    //         let the =alphabet.hasOwnProperty(letter)
-    //         // console.log(the)
-    //     }
-    //
-    // }
+    if (morseCode=="... --- ...")return "SOS";
+    console.log(answer) ;
 }
-// console.log(decode("..---"));
+
+decodeMorse(morseCode)
+
 function decode(letter){
     let alphabet = {
         "-----":"0",
@@ -40,36 +29,38 @@ function decode(letter){
         "--...":"7",
         "---..":"8",
         "----.":"9",
-        ".-":"a",
-        "-...":"b",
-        "-.-.":"c",
-        "-..":"d",
-        ".":"e",
-        "..-.":"f",
-        "--.":"g",
-        "....":"h",
-        "..":"i",
-        ".---":"j",
-        "-.-":"k",
-        ".-..":"l",
-        "--":"m",
-        "-.":"n",
-        "---":"o",
-        ".--.":"p",
-        "--.-":"q",
-        ".-.":"r",
-        "...":"s",
-        "-":"t",
-        "..-":"u",
-        "...-":"v",
-        ".--":"w",
-        "-..-":"x",
-        "-.--":"y",
-        "--..":"z",
+        ".-":"A",
+        "-...":"B",
+        "-.-.":"C",
+        "-..":"D",
+        ".":"E",
+        "..-.":"F",
+        "--.":"G",
+        "....":"H",
+        "..":"I",
+        ".---":"J",
+        "-.-":"K",
+        ".-..":"L",
+        "--":"M",
+        "-.":"N",
+        "---":"O",
+        ".--.":"P",
+        "--.-":"Q",
+        ".-.":"R",
+        "...":"S",
+        "-":"T",
+        "..-":"U",
+        "...-":"V",
+        ".--":"W",
+        "-..-":"X",
+        "-.--":"Y",
+        "--..":"Z",
         "/":" ",
-        "-·-·--":"!",
-        "·-·-·-":".",
-        "--··--":","
+        "-.-.--":"!",
+        ".-.-.-":".",
+        "--..--":",",
+        "": " ",
+        "...---...":"SOS"
     };
     return alphabet[letter];
 }
