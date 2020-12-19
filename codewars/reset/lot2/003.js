295c487b18dd47462014ed28f23dd36c7fce5aac
pxmihai <pxmihai@yahoo.com>
2020-12-20 01:31:39 +0200

    - stuff

--- codewars/reset/lot2/003.js
+++ codewars/reset/lot2/003.js
@@ -1,174 +1,192 @@
-class left{
-    constructor (f1=12,f2=24,f3=48,f4=84,f5=132,f6="on"){
-        this.a=f1;
-        this.b=f2;
-        this.c=f3;
-        this.d=f4;
-        this.e=f5;
-        this.f=f6;
+class left {
+    constructor(f1 = 12, f2 = 24, f3 = 48, f4 = 84, f5 = 132, f6 = "on") {
+        this.a = f1;
+        this.b = f2;
+        this.c = f3;
+        this.d = f4;
+        this.e = f5;
+        this.f = f6;
     }
 
-    dataStore1={
-        "ax":"xab",
-        "ay":"xac",
-        "az":"xad",
-        "ex1":{
-            "bx":"xbb",
-            "by":"xn"
+    dataStore1 = {
+        "ax": "xab",
+        "ay": "xac",
+        "az": "xad",
+        "ex1": {
+            "bx": "xbb",
+            "by": "xn"
         }
     }
-    dataStore2={
-        a:0,
-        b:1,
-        c:2,
-        d:[{e:3, f:4}],
+    dataStore2 = {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: [{e: 3, f: 4}],
     }
-    dataStore3=[
-        {sx:24,
-            1234:24213}
+    dataStore3 = [
+        {
+            sx: 24,
+            1234: 24213
+        },
+        [1, 2, 3, 4, 5]
     ]
-    dataStore4={
+    dataStore4 = {
 
-1:"ACÉROLA (cerise des Antilles)",
-2:"Acerola – West Indian Cherry",
-3:"Pomme",
-4:"Apple",
-5:"Apricots",
-6:"Avocado",
-7:"Banana",
-8:"Blackberries",
-9:"Blackcurrant",
-10:"Blueberries",
-11:"Breadfruit",
-12:"Cantaloupe",
-13:"Carambola",
-14:"Cherimoya",
-15:"Cherries",
-16:"Clementine",
-17:"Coconut Meat",
-18:"Cranberries",
-19:"Custard-Apple",
-20:"Date Fruit",
-21:"Durian",
-22:"Elderberries",
-23:"Feijoa",
-24:"Figs",
-25:"Gooseberries",
-26:"Grapefruit",
-27:"Grapes",
-28:"Guava",
-29:"Honeydew Melon",
-30:"Jackfruit",
-31:"Java-Plum",
-32:"Jujube Fruit",
-33:"Kiwifruit",
-34:"Kumquat",
-34:"Lemon",
-36:"lime",
-37:"Lime",
-38:"Longan",
-39:"Loquat",
-40:"Lychee",
-41:"Mandarin",
-41:"Mango",
-44:"Mangosteen",
-45:"Mulberries",
-46:"Nectarine",
-47:"Olives",
-48:"Orange",
-49:"Papaya",
-50:"Passion Fruit",
-51:"Peaches",
-52:"Pear",
-53:"Persimmon – Japanese",
-54:"Pitaya (Dragonfruit)",
-55:"Pineapple",
-56:"Pitanga",
-57:"Plantain",
-58:"Plums",
-59:"Pomegranate",
-60:"Prickly Pear",
-61:"Prunes",
-62:"Pummelo",
-63:"Quince",
-64:"Raspberries",
-65:"Rhubarb",
-66:"Rose-Apple",
-67:"Sapodilla",
-68:"Sapote, Mamey",
-69:"Soursop",
-70:"Strawberries",
-71:"Sugar-Apple",
-72:"Tamarind",
-73:"Tangerine",
-74:"Watermelon",
+        1: "ACÉROLA (cerise des Antilles)",
+        2: "Acerola – West Indian Cherry",
+        3: "Pomme",
+        4: "Apple",
+        5: "Apricots",
+        6: "Avocado",
+        7: "Banana",
+        8: "Blackberries",
+        9: "Blackcurrant",
+        10: "Blueberries",
+        11: "Breadfruit",
+        12: "Cantaloupe",
+        13: "Carambola",
+        14: "Cherimoya",
+        15: "Cherries",
+        16: "Clementine",
+        17: "Coconut Meat",
+        18: "Cranberries",
+        19: "Custard-Apple",
+        20: "Date Fruit",
+        21: "Durian",
+        22: "Elderberries",
+        23: "Feijoa",
+        24: "Figs",
+        25: "Gooseberries",
+        26: "Grapefruit",
+        27: "Grapes",
+        28: "Guava",
+        29: "Honeydew Melon",
+        30: "Jackfruit",
+        31: "Java-Plum",
+        32: "Jujube Fruit",
+        33: "Kiwifruit",
+        34: "Kumquat",
+        35: "Lemon",
+        36: "lime",
+        37: "Lime",
+        38: "Longan",
+        39: "Loquat",
+        40: "Lychee",
+        41: "Mandarin",
+        42: "Mango",
+        44: "Mangosteen",
+        45: "Mulberries",
+        46: "Nectarine",
+        47: "Olives",
+        48: "Orange",
+        49: "Papaya",
+        50: "Passion Fruit",
+        51: "Peaches",
+        52: "Pear",
+        53: "Persimmon – Japanese",
+        54: "Pitaya (Dragonfruit)",
+        55: "Pineapple",
+        56: "Pitanga",
+        57: "Plantain",
+        58: "Plums",
+        59: "Pomegranate",
+        60: "Prickly Pear",
+        61: "Prunes",
+        62: "Pummelo",
+        63: "Quince",
+        64: "Raspberries",
+        65: "Rhubarb",
+        66: "Rose-Apple",
+        67: "Sapodilla",
+        68: "Sapote, Mamey",
+        69: "Soursop",
+        70: "Strawberries",
+        71: "Sugar-Apple",
+        72: "Tamarind",
+        73: "Tangerine",
+        74: "Watermelon",
     }
-    dataStore5={
-        A: ["Apples", "Apricots", "Avocados"]
+    dataStore5 = {
+        A: ["Apples", "Apricots", "Avocados"],
 
         B: ["Bananas", "Boysenberries", "Blueberries", "Bing Cherry"],
 
-        C: [Cherries, Cantaloupe, Crab apples, Clementine, Cucumbers],
+        C: ["Cherries", "Cantaloupe", "Crab apples", "Clementine", "Cucumbers"],
 
-        D: [Damson plum, Dinosaur Eggs (Pluots), Dates, Dewberries, Dragon Fruit],
+        D: ["Damson plum", "Dinosaur Eggs (Pluots)", "Dates", "Dewberries", "Dragon Fruit"],
 
-        E: [Elderberry, Eggfruit, Evergreen Huckleberry, Entawak],
+        E: ["Elderberry", "Eggfruit", "Evergreen Huckleberry", "Entawak"],
 
-        F: [Fig, Farkleberry, Finger Lime],
+        F: ["Fig", "Farkleberry", "Finger Lime"],
 
-        G: [Grapefruit, Grapes, Gooseberries, Guava],
+        G: ["Grapefruit", "Grapes", "Gooseberries", "Guava"],
 
-        H: [Honeydew melon, Hackberry, Honeycrisp Apples],
+        H: ["Honeydew melon", "Hackberry", "Honeycrisp Apples"],
 
-        I: [Indian Prune (Plum), Indonesian Lime, Imbe, Indian Fig],
+        I: ["Indian Prune (Plum)", "Indonesian Lime", "Imbe", "Indian Fig"],
 
-        J: [Jackfruit, Java Apple, Jambolan],
+        J: ["Jackfruit", "Java Apple", "Jambolan"],
 
-        K: [Kiwi, Kaffir Lime, Kumquat],
+        K: ["Kiwi", "Kaffir Lime", "Kumquat"],
 
-        L: [Lime (Lemon), Longan, Lychee, Loquat],
+        L: ["Lime (Lemon)", "Longan", "Lychee", "Loquat"],
 
-        M: [Mango, Mandarin Orange, Mulberry, Melon],
+        M: ["Mango", "Mandarin Orange", "Mulberry", "Melon"],
 
-        N: [Nectarine, Navel Orange, Nashi Pear (Asian Pear)],
+        N: ["Nectarine", "Navel Orange", "Nashi Pear (Asian Pear)"],
 
-        O: [Olive, Oranges, Ogeechee Limes, Oval Kumquat],
+        O: ["Olive", "Oranges", "Ogeechee Limes", "Oval Kumquat"],
 
-        P: [Papaya, Persimmon, Paw Paw, Prickly Pear, Peach, Pomegranate, Pineapple],
+        P: ["Papaya", "Persimmon", "Paw Paw", "Prickly Pear", "Peach", "Pomegranate", "Pineapple"],
 
-        Q: [Quince, Queen Anne Cherry, Quararibea cordata (Chupa Chupa)],
+        Q: ["Quince, Queen Anne Cherry", "Quararibea cordata (Chupa Chupa)"],
 
-        R: [Rambutan, Raspberries, Rose Hips],
+        R: ["Rambutan", "Raspberries", "Rose Hips"],
 
-        S: [Star Fruit, Strawberries, Sugar Baby Watermelon],
+        S: ["Star Fruit", "Strawberries", "Sugar Baby Watermelon"],
 
-        T: [Tomato, Tangerine, Tamarind, Tart Cherries],
+        T: ["Tomato", "Tangerine", "Tamarind", "Tart Cherries"],
 
-        U: [Ugli Fruit, Uniq Fruit, Ugni],
+        U: ["Ugli Fruit", "Uniq Fruit", "Ugni"],
 
-        V: [Vanilla Bean, Velvet Pink Banana, Voavanga],
+        V: ["Vanilla Bean", "Velvet Pink Banana", "Voavanga"],
 
-        W: [Watermelon, Wolfberry, White Mulberry],
+        W: ["Watermelon", "Wolfberry", "White Mulberry"],
 
-        X: [Xigua (Chinese Watermelon), Ximenia caffra fruit, Xango Mangosteen Fruit Juice],
+        X: ["Xigua (Chinese Watermelon)", "Ximenia caffra fruit", "Xango Mangosteen Fruit Juice"],
 
-        Y: [Yellow Passion Fruit, Yunnan Hackberry, Yangmei],
+        Y: ["Yellow Passion Fruit", "Yunnan Hackberry", "Yangmei"],
 
-        Z: [Zig Zag Vine fruit, Zinfandel Grapes, Zucchini (a fruit, like tomatoes)],
+        Z: ["Zig Zag Vine fruit", "Zinfandel Grapes", "Zucchini (a fruit, like tomatoes)"],
     }
 
-    dataSlot6="what6";
-    dataSlot7="what7";
-    dataSlot8="what8";
-    dataSlot9="what9";
+    dataStore6 = "what6";
+    dataStore7 = "what7";
+    dataStore8 = "what8";
+    dataStore9 = "what9";
+    dataStore10 = {
+        maybe() {
+            console.log("Works!")
+        }
+    }
 
-    dance(){ return console.log(this.random1)}
-    walk(){ return this.swirl1.ax}
-    talk(){ return this.c}
+    dance() {
+        return console.log(this.dataStore1)
+    }
+
+    walk() {
+        return console.log(this.dataStore2)
+    }
+
+    talk() {
+        return console.log(this.dataStore3)
+    }
 
 }
 
-let elementOne= new left();
+let elementOne = new left();
+
+let
 
 
 
--- codewars/reset/lot2/scratch.js
+++ codewars/reset/lot2/scratch.js
@@ -1,11 +1,45 @@
-let me=()=>[9,8,7,6,5];
-    // return [9,8,7,6,5];
+// let me=()=>[9,8,7,6,5];
+//     // return [9,8,7,6,5];
+//
+// const [marry,had,a,little,lamb]=me();
+// console.log(marry,had,a,little,lamb)
+// let us=[1,2,3,4,5]
+// const[fleece,was,white,as,snow]=us;
+// console.log(fleece,was,white,as,snow)
+//
+// console.log(typeof(me))
+// console.log(typeof(us))
 
-const [marry,had,a,little,lamb]=me();
-console.log(marry,had,a,little,lamb)
-let us=[1,2,3,4,5]
-const[fleece,was,white,as,snow]=us;
-console.log(fleece,was,white,as,snow)
+// const [water,fire]=[3,2,1];
+// console.log(water);
+// console.log(fire);
 
-console.log(typeof(me))
-console.log(typeof(us))
+let spaceBucket = {
+    value0: "pair",
+    value1: {value00: "pair0"},
+    value4: {value02: "pair2", value03: "pair3"},
+    value5: [
+        {value05: "pair5", value06: "pair6"},
+        {value07: "pair7", value08: "pair8"}
+    ],
+    value6:[ [{a: "x"}],[{b: "y"}],[{c: "z"}],[{d: "x"}],[{e: "y"}],[{f: "z"}]
+    ],
+
+
+}
+
+let objects ={pick:[{a:"abc"},{b:"bcd"},{c:"cde"},{d:"def"}]};
+console.log(objects.pick.find((item) => item.hasOwnProperty('d')).d)
+
+// console.log(spaceBucket.value)/*undefined*/
+// console.log(spaceBucket.value0)/*pair*/
+// console.log(spaceBucket.value1)/*{ value00: 'pair0' }*/
+// console.log(spaceBucket.value1.value00)/*pair0*/
+// console.log(spaceBucket.value5[0])/*{ value05: 'pair5', value06: 'pair6' }*/
+// console.log(spaceBucket.value5[0].value05)/*pair5*/
+// console.log(spaceBucket.value5[0].value06)/*pair6*/
+// console.log(spaceBucket.value5[0].value05)/*pair5*/
+// console.log(spaceBucket.value5[0].value06)/*pair6*/
+// console.log(spaceBucket.value5[1].value07) /*pair6*/
+// console.log(spaceBucket.value6[3][0].d) /* x */
+console.log(spaceBucket.value6.find((item) => item.hasOwnProperty('d')))
